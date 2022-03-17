ba476c13b760c48e1cfc3bda9fb920df5904625d
solmoyano <solmoyano1012@gmail.com>
2022-03-17 20:56:57 -0300

    second commit

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function NavBar(){
+
+    return(
+        <header>
+            <div>
+                <ul>
+                    <li>Mochilas</li>
+                    <li>Carteras</li>
+                    <li>Riñoneras</li>
+                </ul>
+            </div>
+        </header>
+    )
+}
+
+export default NavBar;
